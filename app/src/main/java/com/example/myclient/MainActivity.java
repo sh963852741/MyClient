5fc7fbbb9d092c19eeb3e4e8ced4bce08799bad9
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-05-04 13:48:05 +0800

    可以发送文件了

--- app/src/main/java/com/example/myclient/MainActivity.java
+++ app/src/main/java/com/example/myclient/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myclient;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +16,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.net.Socket;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
 import droidninja.filepicker.utils.ContentUriUtils;
 
 public class MainActivity extends AppCompatActivity
@@ -36,18 +44,13 @@ public class MainActivity extends AppCompatActivity
     // 用于将从服务器获取的消息显示出来
     private Handler mMainHandler;
 
+    ArrayList<Uri> filePaths=new ArrayList<Uri>();
     // Socket变量
     private Socket socket;
 
     // 线程池
     // 为了方便展示,此处直接采用线程池进行线程管理,而没有一个个开线程
     private ExecutorService mThreadPool;
-
-    /**
-     * 接收服务器消息 变量
-     */
-    // 输入流对象
-    InputStream is;
 
     // 输入流读取器对象
     InputStreamReader isr ;
@@ -56,12 +59,6 @@ public class MainActivity extends AppCompatActivity
     // 接收服务器发送过来的消息
     String response;
 
-
-    /**
-     * 发送消息到服务器 变量
-     */
-    // 输出流对象
-    OutputStream outputStream;
 
     /**
      * 按钮 变量
@@ -136,7 +133,7 @@ public class MainActivity extends AppCompatActivity
         Receive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openFile();
+                SelectFile();
             }
         });
         /**
@@ -150,48 +147,8 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public void run() {
                         try {
-                            outputStream = socket.getOutputStream();
-                            InputStream input=getResources().openRawResource(R.raw.test);
-                            Reader reader=new InputStreamReader(input);
-                            StringBuffer stringBuffer=new StringBuffer();
-                            int length=input.available();
-                            System.out.println(length);
-                            int groupLength=255;
-                            int count=length/groupLength+1;
-                            String outputstring;
-                            char b[];
-                            for(int i=0;i<count;i++)
-                            {
-                                if(i==count-1) b=new char[length-i*groupLength];
-                                else b=new char[groupLength];
-                                int len=-1;
-                                try
-                                {
-                                    while ((len = reader.read(b))!= -1)
-                                    {
-                                        stringBuffer.append(b);
-                                    }
-                                }
-                                catch (IOException e)
-                                {
-                                    Log.e("ReadingFile","IOException");
-                                }
-                                outputstring=stringBuffer.toString();
-                                System.out.println(outputstring);
-                                outputStream.write((outputstring+"\n").getBytes("utf-8"));
-                                outputStream.flush();
-                            }
-
-                            // 步骤1：从Socket 获得输出流对象OutputStream
-                            // 该对象作用：发送数据
-
-
-                            // 步骤2：写入需要发送的数据到输出流对象中
-                            // 特别注意：数据的结尾加上换行符才可让服务器端的readline()停止阻塞
-
-                            // 步骤3：发送数据到服务端
-
-                        } catch (IOException e) {
+                            SendFile();
+                        } catch (IOException | URISyntaxException e) {
                             e.printStackTrace();
                         }
                     }
@@ -205,10 +162,7 @@ public class MainActivity extends AppCompatActivity
         btnDisconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 try {
-                    // 断开 客户端发送到服务器 的连接，即关闭输出流对象OutputStream
-                    outputStream.close();
 
                     // 断开 服务器发送到客户端 的连接，即关闭输入流读取器对象BufferedReader
                     br.close();
@@ -225,7 +179,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
-    public void openFile()
+    public void SelectFile()
     {
         ArrayList<Uri> filePaths=new ArrayList<Uri>();
         FilePickerBuilder.getInstance().setMaxCount(10)
@@ -233,5 +187,90 @@ public class MainActivity extends AppCompatActivity
                 .pickPhoto(this);
     }
 
+    public void SendFile() throws IOException, URISyntaxException {
+        OutputStream outputStream = null;
+        InputStream intputStream = null;
+        Socket socket=null;
+        try {
+            // 创建Socket对象 & 指定服务端的IP 及 端口号
+            socket = new Socket("192.168.18.3", 8888);
+            outputStream = socket.getOutputStream();
+            intputStream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String path = ContentUriUtils.INSTANCE.getFilePath(this,filePaths.get(0));
+        byte[] recvBuffer = new byte[1024];
+        byte[] sendBuffer = new byte[1024];
+        // 发送文件信息
+        FileInputStream in = new FileInputStream(path);
+        sendBuffer=intToByteArray(3,sendBuffer,0);
+        sendBuffer=intToByteArray(in.available(),sendBuffer,4);
+        byte[] fileNameBytes=path.getBytes();
+        for(int i=0;i<fileNameBytes.length;++i)
+        {
+            sendBuffer[i+12]=fileNameBytes[i];
+        }
+        outputStream.write(sendBuffer);
+        //调整文件指针
+        intputStream.read(recvBuffer);
+        int pos=byteArrayToInt(recvBuffer);
+        in.skip(pos);
+        //开始传输文件
+        int byteRead = 0;
+        while ((byteRead = in.read(sendBuffer)) != -1) {
+            outputStream.write(sendBuffer);
+        }
+        //校验文件
+        intputStream.read(recvBuffer);
+        outputStream.write("finish\0".getBytes());
+        socket.close();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case FilePickerConst.REQUEST_CODE_PHOTO:
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    ArrayList<Uri> dataList = data.getParcelableArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA);
+                    if (dataList != null) {
+                        filePaths.addAll(dataList);
+                    }
+                }
+                break;
+
+            case FilePickerConst.REQUEST_CODE_DOC:
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    ArrayList<Uri> dataList = data.getParcelableArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS);
+                    if (dataList != null) {
+                        filePaths.addAll(dataList);
+                    }
+                }
+                break;
+        }
+        // addThemToView(photoPaths, docPaths);
+    }
+
+    private byte[] intToByteArray(int value,byte[] intput, int fromWhere)
+    {
+        intput[fromWhere+0] = (byte) (value & 0xFF);
+        intput[fromWhere+1] = (byte) (value >> 8  & 0xFF);
+        intput[fromWhere+2] = (byte) (value >> 16 & 0xFF);
+        intput[fromWhere+3] = (byte) (value >> 24 & 0xFF);
+        return intput;
+    }
+
+    private int byteArrayToInt(byte[] byteArray)
+    {
+        if(byteArray.length != 4)
+        {
+            return 0;
+        }
+        int value = byteArray[0] & 0xFF;
+        value |= byteArray[1] << 8;
+        value |= byteArray[2] << 16;
+        value |= byteArray[3] << 24;
+        return value;
+    }
 }
 
